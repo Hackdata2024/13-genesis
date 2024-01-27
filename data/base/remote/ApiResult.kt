d54bd338e3ba07e8f3771487ba9009be4589e43d
Jalaj.S <127764495+jojinkey@users.noreply.github.com>
2024-01-28 02:20:27 +0530

    Add files via upload

---
+++ data/base/remote/ApiResult.kt
@@ -0,0 +1,15 @@
+package com.intelli.data.base.remote
+
+/**
+ * Created by Jalaj on 21-12-2023.
+ */
+sealed interface ApiResult<T> {
+
+    class Success<T>(val data: T) : ApiResult<T>
+
+    class Message<T>(val code: Int, val message: String?) : ApiResult<T>
+
+    class Exception<T>(val throwable: Throwable): ApiResult<T>
+
+
+}
---
+++ data/local/assets/entities/ModelCategoriesList.kt
@@ -0,0 +1,9 @@
+package com.intelli.data.local.assets.entities
+
+/**
+ * Created by Jalaj on 06-11-2023.
+ */
+data class ModelCategoriesList(
+    var items: List<ModelCategory>
+) {
+}
